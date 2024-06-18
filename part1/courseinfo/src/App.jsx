90b0c72495885fa4bf3cd5df7b3128b15cf63eab
g4li0 <gqc768@gmail.com>
2024-06-19 01:06:28 +0200

    part1 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,20 +5,21 @@ const Header = (props) => {
     <h1>{props.course}</h1>
   )
 }
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
   )
 }
 
