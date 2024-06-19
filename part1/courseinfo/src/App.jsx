e98ec809bfe649251a856b2be6abe65053757255
g4li0 <gqc768@gmail.com>
2024-06-20 00:55:40 +0200

    part1 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,47 +16,44 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
 
 const Total = (props) => {
+  //use map to transform the array of objects into an array of numbers, then with reduce we sum all the numbers
   return (
     <p>
-      Number of exercises {props.exercises.reduce((a, b) => a + b, 0)}
+      Number of exercises {props.parts.map(part => part.exercises).reduce((a, b) => a + b, 0)}
     </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const exercises = [part1.exercises, part2.exercises, part3.exercises]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        />
-      <Total exercises={exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
