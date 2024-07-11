31caef1f5723757d6ebdbf4280ecacd7b85daea2
g4li0 <gqc768@gmail.com>
2024-07-11 18:23:36 +0200

    part3 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -14,7 +15,11 @@ app.use(morgan(function (tokens, req, res) {
       tokens['response-time'](req, res), 'ms',
       tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : ''
     ].join(' ')
-  }))
+}))
+
+app.use(cors())
+
+app.use(express.static('dist'))
 
 let persons = [
     {
@@ -96,7 +101,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
