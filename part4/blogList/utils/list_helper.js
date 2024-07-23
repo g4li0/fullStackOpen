bed676fd07f0596573d31a79e5b781ab6455fd41
g4li0 <gqc768@gmail.com>
2024-07-23 22:41:05 +0200

    part4 4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum,blog) => sum + blog.likes,0)
 }
 
+const favoriteBlog = (blogs) => {
+    if(blogs.length<1){
+        return {}
+    }
+    const sorted = blogs.slice().sort((b1,b2) => b2.likes - b1.likes)
+    return sorted[0]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
