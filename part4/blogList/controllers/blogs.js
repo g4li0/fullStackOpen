09bc679fd79b340bdb3eebd0a059f6dd0cff3391
g4li0 <gqc768@gmail.com>
2024-07-28 14:22:49 +0200

    part4 4.13 & 4.14

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -23,4 +23,30 @@ blogsRouter.post('/', async (request, response) => {
     }
 })
 
+blogsRouter.delete('/:id', async (request,response) => {
+    try{
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+    }
+    catch (exception) {
+        response.status(400).send(`error ${exception}`).end()
+    }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    try{
+        const blog = {
+            title: request.body.title,
+            author: request.body.author,
+            url: request.body.url,
+            likes: request.body.likes
+        }
+        const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        response.json(result)
+    }
+    catch (exception) {
+        response.status(400).send(`error ${exception}`).end()
+    }
+})
+
 module.exports = blogsRouter
--- part4/blogList/tests/list_helper.test.js
+++ part4/blogList/tests/list_helper.test.js
@@ -1,12 +1,7 @@
-const { test, describe, after, beforeEach } = require('node:test')
+const { test, describe} = require('node:test')
 const assert = require('node:assert')
-const supertest = require('supertest')
-const mongoose = require('mongoose')
 const listHelper = require('../utils/list_helper')
 const helper = require('./test_helper')
-const app = require('../app')
-
-const Blog = require('../models/blog')
 
 test('dummy returns one', () => {
     const blogs = []
@@ -64,100 +59,5 @@ describe('most likes', () => {
     })
     test('of a bigger list', () => {
         assert.deepStrictEqual(listHelper.mostLikes(helper.blogList), { author: 'Edsger W. Dijkstra', likes: 17 })
-    })
-})
-
-const api = supertest(app)
-
-describe('GET & POST /api/blogs', () => {
-    beforeEach(async () => {
-        await Blog.deleteMany({})
-
-        const blogObjects = helper.blogList.map(blog => new Blog(blog))
-        const promiseArray = blogObjects.map(blog => blog.save())
-        await Promise.all(promiseArray)
-    })
-
-    test('GET blogs: correct Content-Type and length', async () => {
-        const response = await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-        assert.strictEqual(response.body.length, helper.blogList.length)
-
-    })
-
-    test('GET blogs: correct data params', async () => {
-        const response = await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-
-        const allHaveId = response.body.every(blog => blog.id !== undefined)
-        assert.strictEqual(allHaveId, true)
-
-    })
-
-    const newBLog = {
-        title: 'test',
-        author: 'test',
-        url: 'test',
-        likes: 0
-    }
-
-    test('POST blogs: increases blogs by one and new content stored correctly', async () => {
-        const oldBlogs = helper.blogList
-
-        const response = await api
-            .post('/api/blogs')
-            .send(newBLog)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-
-        const newBlogs = await api.get('/api/blogs')
-
-        const found = newBlogs.body.find(blog => blog.id === response.body.id)
-
-        assert.strictEqual(newBlogs.body.length, oldBlogs.length + 1)
-        assert.strictEqual(found.title, newBLog.title)
-        assert.strictEqual(found.author, newBLog.author)
-        assert.strictEqual(found.url, newBLog.url)
-        assert.strictEqual(found.likes, newBLog.likes)
-    })
-
-    test('POST blogs: likes default to 0 if not provided', async () => {
-        const response = await api
-            .post('/api/blogs')
-            .send({
-                title: 'test',
-                author: 'test',
-                url: 'test'
-            })
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-        assert.strictEqual(response.body.likes, 0)
-    })
-
-    test('POST blogs: title and url are required', async () => {
-        await api
-            .post('/api/blogs')
-            .send({})
-            .expect(400)
-        await api
-            .post('/api/blogs')
-            .send({ title: 'test' })
-            .expect(400)
-        await api
-            .post('/api/blogs')
-            .send({ url: 'test' })
-            .expect(400)
-        await api
-            .post('/api/blogs')
-            .send({ title: 'test', url: 'test' })
-            .expect(201)
-    })
-
-    after(async () => {
-        await mongoose.connection.close()
     })
 })
