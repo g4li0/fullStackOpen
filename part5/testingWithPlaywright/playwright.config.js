416af33004b13fddde5c3c13142ac8a07fa053f6
g4li0 <gqc768@gmail.com>
2024-08-18 23:06:36 +0200

    part5 5.20

--- part5/testingWithPlaywright/playwright.config.js
+++ part5/testingWithPlaywright/playwright.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,//process.env.CI ? 1 : undefined,
-  timeout: 30000,
+  timeout: 5000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- part5/testingWithPlaywright/tests/BlogApp.test.js
+++ part5/testingWithPlaywright/tests/BlogApp.test.js
@@ -1,13 +1,31 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-
-const login = async ( page ) => {
-  await page.getByTestId('username').fill('root')
-  await page.getByTestId('password').fill('toor')
-  await page.getByText('login').click()
-}
+const { post } = require('../../../part4/blogList/app')
 
 describe('Blog app', () => {
   const baseUrl = 'http://localhost:5173'
+  const login = async ( page ) => {
+    await page.getByTestId('username').fill('root')
+    await page.getByTestId('password').fill('toor')
+    await page.getByText('login').click()
+  }
+  const postBlog = async ( request ) => {
+    const response = await request.post(`${baseUrl}/api/login`,{
+      data:{
+        username: 'root',
+        password: 'toor'
+      }
+    })
+    await request.post(`${baseUrl}/api/blogs`,{
+      data:{
+        title: 'title',
+        author: 'author',
+        url: 'url'
+      },
+      headers:{
+        Authorization: `bearer ${response.data.token}`
+      }
+    })
+  }
   beforeEach(async ({ page, request }) => {
     await request.post(`${baseUrl}/api/testing/reset`)
     await request.post(`${baseUrl}/api/users`,{ 
@@ -17,6 +35,7 @@ describe('Blog app', () => {
         password: 'toor'
       }
     })
+    
     await page.goto(baseUrl)
   })
 
@@ -65,6 +84,21 @@ describe('Blog app', () => {
       await page.locator('form').locator('input[type="submit"]').click()
       await expect(page.locator('.success')).toContainText('a new blog title by author added')
       await expect(page.getByText('title author')).toBeVisible()
+    })
+
+    test('a blog can be liked', async ({ page }) => {
+      await page.getByText('new blog').click()
+      await page.getByTestId('title').fill('title')
+      await page.getByTestId('author').fill('author')
+      await page.getByTestId('url').fill('url')
+      await page.locator('form').locator('input[type="submit"]').click()
+      await page.getByText('view').click()
+      const likeButton = await page.getByText('like')
+      const data = await page.locator('.hiddenInfo').textContent()
+      const likes = await data.split(' ')[1]
+      await expect(likeButton).toBeVisible()
+      await likeButton.click()
+      await expect(page.locator('.hiddenInfo')).not.toContainText(likes+1)
     })
   })
 
